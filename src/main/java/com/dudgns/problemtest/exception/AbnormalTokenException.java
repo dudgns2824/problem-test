b36b1264b69dcf1e3ef30fc5bde5163347953226
dudgns2824 <dudgns2824@gmail.com>
2024-04-27 00:33:54 +0900

    auth 수정

--- src/main/java/com/dudgns/problemtest/exception/AbnormalTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.dudgns.purchase.exception;
-
-public class AbnormalTokenException extends RuntimeException{
-    public AbnormalTokenException() {
-        super("올바르지 않은 토큰 입니다.");
-    }
-}
