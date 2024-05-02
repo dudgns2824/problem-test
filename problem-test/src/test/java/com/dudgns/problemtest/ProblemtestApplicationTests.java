f0be1e36e29f86caacb180990e1bdb112e8df67d
dudgns2824 <soar0531@cknb.co.kr>
2024-05-02 10:33:11 +0900

    아 테스트 코드 깜빡했다 ㅁㅊ

--- problem-test/src/test/java/com/dudgns/problemtest/ProblemtestApplicationTests.java
+++ problem-test/src/test/java/com/dudgns/problemtest/ProblemtestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.dudgns.problemtest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static reactor.core.publisher.Mono.when;
+
+@WebMvcTest
 class ProblemtestApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    @DisplayName("조회 테스트")
+    void contextLoads() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/problemTest/carManagement"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 
 }
