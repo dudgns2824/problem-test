e914d1990f0845439577c36be32f3ebeb3b92ee3
dudgns2824 <dudgns2824@gmail.com>
2024-05-24 11:11:17 +0900

    제약 조건 설정

--- problem-test/project-car-mgt-api/connector/src/main/kotlin/dudgns/com/backend/carmgt/connector/dto/RequestCarRegistDto.kt
+++ problem-test/project-car-mgt-api/connector/src/main/kotlin/dudgns/com/backend/carmgt/connector/dto/RequestCarRegistDto.kt
@@ -1,15 +1,19 @@
 package dudgns.com.backend.carmgt.connector.dto
 
+import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
 import org.jetbrains.annotations.NotNull
 
 data class RequestCarRegistDto(
     @NotNull
     val categoryTypeList: List<Int>,
     @NotNull
+    @Positive(message = "회사 코드는 무조건 0보다 커야 합니다.")
     val companyCode: Long,
     @NotNull
     val modelName: String,
     @NotNull
+    @Size(min = 1900, message = "생산 년도는 1900년 이상으로 입력 해 주세요.")
     val createdYear: Int,
     @NotNull
     val rentalYn: Boolean
