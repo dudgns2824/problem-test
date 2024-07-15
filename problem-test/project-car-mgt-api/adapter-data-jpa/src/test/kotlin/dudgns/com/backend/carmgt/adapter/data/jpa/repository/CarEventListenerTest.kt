869e6f97d7627e6a6b1ca26f7e7184e0e93fb298
dudgns2824 <dudgns2824@gmail.com>
2024-07-15 16:27:50 +0900

    given when then 패턴 추가

--- problem-test/project-car-mgt-api/adapter-data-jpa/src/test/kotlin/dudgns/com/backend/carmgt/adapter/data/jpa/repository/CarEventListenerTest.kt
+++ problem-test/project-car-mgt-api/adapter-data-jpa/src/test/kotlin/dudgns/com/backend/carmgt/adapter/data/jpa/repository/CarEventListenerTest.kt
@@ -9,6 +9,7 @@ import jakarta.persistence.EntityManager
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.transaction.annotation.Transactional
+import kotlin.test.assertTrue
 
 @AnnotCarManagementRepositoryTest
 class CarEventListenerTest(
@@ -18,45 +19,59 @@ class CarEventListenerTest(
     private val entityManager: EntityManager,
 ) {
     @Test
+    @Transactional(readOnly = false)
     fun `자동차 목록 조회`() {
+        val dto =
+            RegistCarInfoCommand(
+                categoryTypeList = listOf(1, 2),
+                companyCode = 1L,
+                modelName = "테스트 모델",
+                createdYear = 12345,
+                rentalYn = false,
+            )
+
+        val isSuccess = carEventListener.registCarInfo(dto)
+
         val data =
             carEventListener.getCarInfo(
-                GetCarInfoListQueryCommand(null, null, null, null, null),
+                GetCarInfoListQueryCommand(1, false, 12345, 12345, listOf(1, 2)),
             )
+
+        assertTrue { data.isNotEmpty() }
     }
 
     @Test
     @Transactional(readOnly = false)
     fun `자동차 수정`() {
-        val data =
-            carEventListener.modifyCarInfo(
-                ModifyCarInfoCommand(
-                    carIdx = 1,
-                    categoryTypeList = listOf(1, 2),
-                    companyCode = 1L,
-                    modelName = "테스트 모델",
-                    createdYear = 2024,
-                    rentalYn = false,
-                ),
+        val dto =
+            ModifyCarInfoCommand(
+                carIdx = 1,
+                categoryTypeList = listOf(1, 2),
+                companyCode = 1L,
+                modelName = "테스트 모델",
+                createdYear = 2024,
+                rentalYn = false,
             )
 
-        if (!data) throw RuntimeException("데이터 저장에 실패")
+        val isSuccess = carEventListener.modifyCarInfo(dto)
+
+        assertTrue { true }
     }
 
     @Test
     @Transactional(readOnly = false)
     fun `자동차 저장`() {
-        val data =
-            carEventListener.registCarInfo(
-                RegistCarInfoCommand(
-                    categoryTypeList = listOf(1, 2),
-                    companyCode = 1L,
-                    modelName = "테스트 모델",
-                    createdYear = 2024,
-                    rentalYn = false,
-                ),
+        val dto =
+            RegistCarInfoCommand(
+                categoryTypeList = listOf(1, 2),
+                companyCode = 1L,
+                modelName = "테스트 모델",
+                createdYear = 2024,
+                rentalYn = false,
             )
 
-        if (!data) throw RuntimeException("데이터 저장에 실패")
+        val isSuccess = carEventListener.registCarInfo(dto)
+
+        assertTrue { true }
     }
 }
